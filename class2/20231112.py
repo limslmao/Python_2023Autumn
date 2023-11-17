f860719e873e30d7867a7ecf5a05fafc9ffe29a3
limslmao <jamesmau87@gmail.com>
2023-11-17 17:39:31 +0800

    Update 20231112.py

--- class2/20231112.py
+++ class2/20231112.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("/../firebase_token.json")
+cred = credentials.Certificate("../firebase_token.json")
 firebase_admin.initialize_app(cred)
