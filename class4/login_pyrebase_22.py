4176394e553ddca8303a7fa8cc3a470e91500ea7
limslmao <jamesmau87@gmail.com>
2023-12-03 14:20:07 +0800

    update class4

--- class4/login_pyrebase_22.py
+++ class4/login_pyrebase_22.py
@@ -1,8 +1,5 @@
 import pyrebase
-import tkinter as tk
-from tkinter import Label
-from tkinter import Button
-from tkinter import Entry
+from tkinter import *
 
 # // Your web app's Firebase configuration
 config = {
@@ -22,7 +19,7 @@ auth = firebase.auth()
 
 
 # create Label in login Windows
-root = tk.Tk()
+root = Tk()
 
 loginlabel = Label(root, text = 'Login Page')
 accountlabel = Label(root, text = 'Account')
